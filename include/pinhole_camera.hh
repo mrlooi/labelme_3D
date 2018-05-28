e27bb3f0262e9e62056decc8d4da84eed4142ce9
vincentlooi <vincent.looi@dorabot.com>
2018-05-28 21:49:48 +0800

    updated labelme3d

--- include/pinhole_camera.hh
+++ include/pinhole_camera.hh
@@ -4,26 +4,28 @@
 #include <Eigen/Dense>
 
 #include <pcl/common/transforms.h>
+#include <pcl/octree/octree_search.h>  // for occlusion detection
+#include <pcl/surface/texture_mapping.h>  // for occlusion detection
 
 /* 
 You need first 3 parameters that define your camera: the focal length f, and the center of the projection plane: cx, cy. With this you create a 3x3 matrix (I will use matlab syntax):
 
 A = [ f 0 cx;
-      0 f cy;
-      0 0  1 ];
+	  0 f cy;
+	  0 0  1 ];
 You can use something like cx = 0.5 * image_width, cy = 0.5 * image_height, and some value as f = 800 (try some of them to check how the image looks better).
 
 Then, a 3x4 matrix with the transformation from the camera frame to the point cloud frame:
 
 T = [ r11 r12 r13 tx;
-      r21 r22 r23 ty;
-      r31 r32 r33 tz ];
+	  r21 r22 r23 ty;
+	  r31 r32 r33 tz ];
 And finally, your point cloud in homogeneous coordinates, i.e. in a 4xN matrix for a point cloud with N points:
 
 P = [ x1 x2 ... xN;
-      y1 y2 ... yN;
-      z1 z2 ... zN;
-       1  1 ...  1 ];
+	  y1 y2 ... yN;
+	  z1 z2 ... zN;
+	   1  1 ...  1 ];
 Now you can project the points:
 
 S = A * T * P;
@@ -36,7 +38,9 @@ y = S(2, i) / S(3, i);
 template <typename PointT>
 class PinholeCamera
 {
-    typedef typename pcl::PCLBase<PointT>::PointCloudConstPtr PointCloudConstPtr;
+	typedef typename pcl::PCLBase<PointT>::PointCloudConstPtr PointCloudConstPtr;
+	typedef typename pcl::octree::OctreePointCloudSearch<PointT> Octree;
+	typedef typename Octree::Ptr OctreePtr;
 
 public:
 	PinholeCamera(float focal, float cx, float cy, const Eigen::Matrix4f& camera_pose = Eigen::Matrix4f::Identity()):
@@ -77,64 +81,64 @@ public:
 		camera_pose_ = camera_pose;
 	}
 
-    inline void set_input_cloud(const PointCloudConstPtr &cloud)
-    {
-        cloud_ = cloud;
+	inline void set_input_cloud(const PointCloudConstPtr &cloud)
+	{
+		cloud_ = cloud;
 
-        has_cloud = true;
-    }
+		has_cloud = true;
+	}
 
-    // getter methods
-    inline float get_focal() const
-    {
-    	return f_;
-    }
-    inline float get_cx() const
-    {
-    	return cx_;
-    }
-    inline float get_cy() const
-    {
-    	return cy_;
-    }
-    inline int get_image_width() const
-    {
-    	return image_width_;
-    }
-    inline int get_image_height() const
-    {
-    	return image_height_;
-    }
-    inline size_t get_cloud_size() const
-    {
-    	return cloud_->size();
-    }
-    inline Eigen::Matrix4f get_camera_pose() const
-    {
-    	return camera_pose_;
-    }
+	// getter methods
+	inline float get_focal() const
+	{
+		return f_;
+	}
+	inline float get_cx() const
+	{
+		return cx_;
+	}
+	inline float get_cy() const
+	{
+		return cy_;
+	}
+	inline int get_image_width() const
+	{
+		return image_width_;
+	}
+	inline int get_image_height() const
+	{
+		return image_height_;
+	}
+	inline size_t get_cloud_size() const
+	{
+		return cloud_->size();
+	}
+	inline Eigen::Matrix4f get_camera_pose() const
+	{
+		return camera_pose_;
+	}
 
 
-    void project(cv::Mat& proj_img, Eigen::MatrixXi& uv_idx_map)
-    {
-    	if (!has_cloud)
-    	{
-    		printf("Cloud has not yet been added!\n");
-    		return;
-    	}
-    	compute_A();
-    	compute_T();
-    	compute_P();
+	void project(cv::Mat& proj_img, Eigen::MatrixXi& uv_idx_map)
+	{
+		if (!has_cloud)
+		{
+			printf("Cloud has not yet been added!\n");
+			return;
+		}
+		compute_A();
+		compute_T();
+		compute_P();
 
-    	Eigen::MatrixXf S = A_ * T_ * P_;
+		Eigen::MatrixXf S = A_ * T_ * P_;
 
-    	proj_img = cv::Mat::zeros(image_height_, image_width_, CV_8UC3);
-    	uv_idx_map = Eigen::MatrixXi::Constant(image_height_, image_width_, -1);
+		proj_img = cv::Mat::zeros(image_height_, image_width_, CV_8UC3);
+		uv_idx_map = Eigen::MatrixXi::Constant(image_height_, image_width_, -1);
 
 		typename pcl::PointCloud<PointT>::Ptr rot_cloud (new pcl::PointCloud<PointT>);
 		pcl::transformPointCloud(*cloud_, *rot_cloud, camera_pose_);
 
-    	const int N = cloud_->size();
+		const int N = cloud_->size();
 		// store a cache of the distance of all the points to origin
 		std::vector<float> pts_distance_cache(N);
 		for (int i = 0; i < N; ++i)
@@ -152,54 +156,63 @@ public:
 				continue;
 
 			const PointT& pt = cloud_->points[i];
-			auto& px = proj_img.at<cv::Vec3b>(y,x);
 			if (!pt_has_nan(pt))
 			{
-				px[0] = pt.b;
-				px[1] = pt.g;
-				px[2] = pt.r;
-
 				// printf("%.3f %.3f,", y, x);
+				bool updated = false;
 				int prev_map_idx = uv_idx_map(y, x);
 				if (prev_map_idx == -1)
 				{
 					uv_idx_map(y, x) = i;
+					updated = true;
 				} else {
 					// if another point has the same uv mapping pixel, compare distance from camera to point and use the closest 
-					uv_idx_map(y, x) = pts_distance_cache[i] < pts_distance_cache[prev_map_idx] ? i : prev_map_idx;
+					if (pts_distance_cache[i] < pts_distance_cache[prev_map_idx])
+					{
+						uv_idx_map(y, x) = i;	
+						updated = true;
+					}
+				}
+
+				if (updated)
+				{			
+					auto& px = proj_img.at<cv::Vec3b>(y,x);	
+					px[0] = pt.b;
+					px[1] = pt.g;
+					px[2] = pt.r;
 				}
 			}
 		}
-    }
+	}
 
-    void project(cv::Mat& proj_img, std::vector<std::vector<std::vector<int>>>& uv_idx_map)
-    {
-    	if (!has_cloud)
-    	{
-    		printf("Cloud has not yet been added!\n");
-    		return;
-    	}
-    	compute_A();
-    	compute_T();
-    	compute_P();
+	void project(cv::Mat& proj_img, std::vector<std::vector<std::vector<int>>>& uv_idx_map)
+	{
+		if (!has_cloud)
+		{
+			printf("Cloud has not yet been added!\n");
+			return;
+		}
+		compute_A();
+		compute_T();
+		compute_P();
 
-    	Eigen::MatrixXf S = A_ * T_ * P_;
+		Eigen::MatrixXf S = A_ * T_ * P_;
 
-    	proj_img = cv::Mat::zeros(image_height_, image_width_, CV_8UC3);
-    	uv_idx_map.resize(image_height_);
-    	for (int y = 0; y < image_height_; ++y)
-    	{
-    		uv_idx_map[y].resize(image_width_);
-	    	for (int x = 0; x < image_width_; ++x)
-    		{
-    			uv_idx_map[y][x] = {};
-    		}
-    	}
+		proj_img = cv::Mat::zeros(image_height_, image_width_, CV_8UC3);
+		uv_idx_map.resize(image_height_);
+		for (int y = 0; y < image_height_; ++y)
+		{
+			uv_idx_map[y].resize(image_width_);
+			for (int x = 0; x < image_width_; ++x)
+			{
+				uv_idx_map[y][x] = {};
+			}
+		}
 
 		typename pcl::PointCloud<PointT>::Ptr rot_cloud (new pcl::PointCloud<PointT>);
 		pcl::transformPointCloud(*cloud_, *rot_cloud, camera_pose_);
 
-    	const int N = cloud_->size();
+		const int N = cloud_->size();
 		for (int i = 0; i < N; ++i)
 		{
 			// std::cout << S(0, i) << std::endl;
@@ -210,14 +223,14 @@ public:
 				continue;
 
 			const PointT& pt = cloud_->points[i];
-			auto& px = proj_img.at<cv::Vec3b>(y,x);
 			if (!pt_has_nan(pt))
 			{
+				uv_idx_map[y][x].push_back(i);
+
+				auto& px = proj_img.at<cv::Vec3b>(y,x);
 				px[0] = pt.b;
 				px[1] = pt.g;
 				px[2] = pt.r;
-
-				uv_idx_map[y][x].push_back(i);
 			}
 		}
 	}
@@ -225,34 +238,34 @@ public:
 	/* 
 
 	*/
-    void project_surface(cv::Mat& proj_img, std::vector<std::vector<std::vector<int>>>& uv_idx_map, float dist_tolerance=0)
-    {
-    	if (!has_cloud)
-    	{
-    		printf("Cloud has not yet been added!\n");
-    		return;
-    	}
-    	compute_A();
-    	compute_T();
-    	compute_P();
+	void project_surface(cv::Mat& proj_img, std::vector<std::vector<std::vector<int>>>& uv_idx_map, float dist_tolerance=0)
+	{
+		if (!has_cloud)
+		{
+			printf("Cloud has not yet been added!\n");
+			return;
+		}
+		compute_A();
+		compute_T();
+		compute_P();
 
-    	Eigen::MatrixXf S = A_ * T_ * P_;
+		Eigen::MatrixXf S = A_ * T_ * P_;
 
-    	proj_img = cv::Mat::zeros(image_height_, image_width_, CV_8UC3);
-    	uv_idx_map.resize(image_height_);
-    	for (int y = 0; y < image_height_; ++y)
-    	{
-    		uv_idx_map[y].resize(image_width_);
-	    	for (int x = 0; x < image_width_; ++x)
-    		{
-    			uv_idx_map[y][x] = {};
-    		}
-    	}
+		proj_img = cv::Mat::zeros(image_height_, image_width_, CV_8UC3);
+		uv_idx_map.resize(image_height_);
+		for (int y = 0; y < image_height_; ++y)
+		{
+			uv_idx_map[y].resize(image_width_);
+			for (int x = 0; x < image_width_; ++x)
+			{
+				uv_idx_map[y][x] = {};
+			}
+		}
 
 		typename pcl::PointCloud<PointT>::Ptr rot_cloud (new pcl::PointCloud<PointT>);
 		pcl::transformPointCloud(*cloud_, *rot_cloud, camera_pose_);
 
-    	const int N = cloud_->size();
+		const int N = cloud_->size();
 		// store a cache of the distance of all the points to origin
 		std::vector<float> pts_distance_cache(N);
 		for (int i = 0; i < N; ++i)
@@ -260,8 +273,8 @@ public:
 			pts_distance_cache[i] = get_pt_distance(rot_cloud->points[i]);
 		}
 
-    	// Store a cache (as Matrix) of the minimum point distance for each pixel point
-    	Eigen::MatrixXf uv_min_distance_cache = Eigen::MatrixXf::Constant(image_height_, image_width_, -1); 
+		// Store a cache (as Matrix) of the minimum point distance for each pixel point
+		Eigen::MatrixXf uv_min_distance_cache = Eigen::MatrixXf::Constant(image_height_, image_width_, -1); 
 
 		for (int i = 0; i < N; ++i)
 		{
@@ -273,15 +286,12 @@ public:
 				continue;
 
 			const PointT& pt = cloud_->points[i];
-			auto& px = proj_img.at<cv::Vec3b>(y,x);
 			if (!pt_has_nan(pt))
 			{
-				px[0] = pt.b;
-				px[1] = pt.g;
-				px[2] = pt.r;
-
 				float pt_dist = pts_distance_cache[i];
 				float min_cache_dist = uv_min_distance_cache(y,x);
+
+				bool updated = false;
 				if (min_cache_dist == -1 || pt_dist < min_cache_dist)
 				{
 					uv_min_distance_cache(y,x) = pt_dist;
@@ -298,10 +308,76 @@ public:
 						}
 					}
 					cur_indices.push_back(i);
+
+					auto& px = proj_img.at<cv::Vec3b>(y,x);	
+					px[0] = pt.b;
+					px[1] = pt.g;
+					px[2] = pt.r;
 				} else if (pt_dist < min_cache_dist + dist_tolerance)
 				{
 					// add 
 					uv_idx_map[y][x].push_back(i);
+				}
+			}
+		}
+	}
+
+	void project_non_occluded_surface(cv::Mat& proj_img, std::vector<std::vector<std::vector<int>>>& uv_idx_map, float octree_resolution=0.02)
+	{
+		if (!has_cloud)
+		{
+			printf("Cloud has not yet been added!\n");
+			return;
+		}
+		compute_A();
+		compute_T();
+		compute_P();
+
+		Eigen::MatrixXf S = A_ * T_ * P_;
+
+		proj_img = cv::Mat::zeros(image_height_, image_width_, CV_8UC3);
+		uv_idx_map.resize(image_height_);
+		for (int y = 0; y < image_height_; ++y)
+		{
+			uv_idx_map[y].resize(image_width_);
+			for (int x = 0; x < image_width_; ++x)
+			{
+				uv_idx_map[y][x] = {};
+			}
+		}
+
+		typename pcl::PointCloud<PointT>::Ptr rot_cloud (new pcl::PointCloud<PointT>);
+		pcl::transformPointCloud(*cloud_, *rot_cloud, camera_pose_);
+
+		OctreePtr octree (new Octree(octree_resolution));
+		octree->setInputCloud (rot_cloud);
+		octree->addPointsFromInputCloud ();
+
+		const int N = cloud_->size();
+
+		for (int i = 0; i < N; ++i)
+		{
+			// std::cout << S(0, i) << std::endl;
+			float x = S(0, i) / S(2, i);
+			float y = S(1, i) / S(2, i);
+
+			if (x >= image_width_ || x < 0 || y >= image_height_ || y < 0)
+				continue;
+
+			const PointT& pt = cloud_->points[i];
+			if (!pt_has_nan(pt))
+			{
+				// check occlusion
+				bool is_occ = tm_.isPointOccluded(rot_cloud->points[i], octree);
+				if (!is_occ)
+				{
+					uv_idx_map[y][x].push_back(i);
+
+					// TODO: only update pixel to closest point 
+					auto& px = proj_img.at<cv::Vec3b>(y,x);
+					px[0] = pt.b;
+					px[1] = pt.g;
+					px[2] = pt.r;
 				}
 			}
 		}
@@ -366,6 +442,9 @@ private:
 
 	// state
 	bool has_cloud = false;
+
+	// algorithms
+	typename pcl::TextureMapping<PointT> tm_;
 };
 
 #endif
--- test/labelme_3d.cpp
+++ test/labelme_3d.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
+#include <stack>
 
 #include <pcl/io/pcd_io.h>
 
 #include <pcl/visualization/pcl_visualizer.h>
 #include <pcl/filters/extract_indices.h>
+#include <pcl/console/parse.h>
 
 #include <opencv2/opencv.hpp>
 
@@ -19,12 +21,17 @@ const cv::Scalar RED {0,0,255};
 const cv::Scalar GREEN {0,255,0};
 const cv::Scalar BLUE {255,0,0};
 
+float octree_resolution = 0.02f;
+
 int img_width = 960;
 int img_height = 960;
+int focal = 800;
 
 
 pcl::PointCloud<PointT>::Ptr raw_cloud (new pcl::PointCloud<PointT>);
 pcl::PointCloud<PointT>::Ptr cloud (new pcl::PointCloud<PointT>);
+
+std::string save_file;
 
 // Eigen::MatrixXi uv_idx_map;
 std::vector<std::vector<std::vector<int>>> uv_idx_map;
@@ -35,10 +42,32 @@ std::vector<cv::Point> img_pts;
 
 Eigen::Matrix4f viewer_pose;
 
+std::vector<pcl::PointCloud<PointT>::Ptr> undo_clouds;
+std::vector<pcl::PointCloud<PointT>::Ptr> redo_clouds;
+
+void run_undo_redo(std::vector<pcl::PointCloud<PointT>::Ptr>& stack1, std::vector<pcl::PointCloud<PointT>::Ptr>& stack2)
+{
+	if (stack1.size() == 0)
+		return;
+	pcl::PointCloud<PointT>::Ptr cur_cloud (new pcl::PointCloud<PointT>);
+	*cur_cloud += *cloud;
+	stack2.push_back(cur_cloud);
+	cloud = stack1.back();	
+	stack1.pop_back();
+}
+void undo()
+{
+	run_undo_redo(undo_clouds, redo_clouds);
+}
+void redo()
+{
+	run_undo_redo(redo_clouds, undo_clouds);
+}
+
+
 void project()
 {
 
-  int focal = 800;
 
   float cx = 0.5 * img_width;
   float cy = 0.5 * img_height;
@@ -50,31 +79,33 @@ void project()
   pinhole_cam.set_camera_pose(viewer_pose);
   pinhole_cam.set_input_cloud(cloud);
 
-  pinhole_cam.project_surface(proj_img, uv_idx_map, 0.1);
+  // pinhole_cam.project_surface(proj_img, uv_idx_map, 0.03);
+  pinhole_cam.project(proj_img, uv_idx_map);
+  // pinhole_cam.project_non_occluded_surface(proj_img, uv_idx_map, octree_resolution);
   proj_img_copy = proj_img.clone();
   
 }
 
 void drawPoints(cv::Mat& img_out, const std::vector<cv::Point>& pts)
 {
-    // img_out = img.clone();
-    float radius = 2;
-    const size_t total_pts = pts.size();
-    for (int i = 0; i < total_pts; ++i)
-    {
-        const auto& pt = pts[i];
-        cv::circle( img_out, pt, radius, RED, 3, 8, 0 );
-        if (i > 0)
-        	cv::line( img_out, pts[i-1], pt, cv::Scalar(0,255,0), 1, 8 );	
-        // cv::putText(img_out, std::to_string(i), {bbox.x,bbox.y}, cv::FONT_HERSHEY_SCRIPT_SIMPLEX, 1, color, 2, 8);
-    }
-    // if (total_pts >= 3)
-    // {
+	// img_out = img.clone();
+	float radius = 2;
+	const size_t total_pts = pts.size();
+	for (int i = 0; i < total_pts; ++i)
+	{
+		const auto& pt = pts[i];
+		cv::circle( img_out, pt, radius, RED, 3, 8, 0 );
+		if (i > 0)
+			cv::line( img_out, pts[i-1], pt, cv::Scalar(0,255,0), 1, 8 );	
+		// cv::putText(img_out, std::to_string(i), {bbox.x,bbox.y}, cv::FONT_HERSHEY_SCRIPT_SIMPLEX, 1, color, 2, 8);
+	}
+	// if (total_pts >= 3)
+	// {
 	   //  for (int i = 0; i < total_pts; ++i)
-    // 	{
-    // 		cv::line( img_out, pts[i], pts[(i+1)%total_pts], cv::Scalar(0,255,0), 1, 8 );	
-    // 	}
-    // }
+	// 	{
+	// 		cv::line( img_out, pts[i], pts[(i+1)%total_pts], cv::Scalar(0,255,0), 1, 8 );	
+	// 	}
+	// }
 }
 
 
@@ -84,10 +115,10 @@ static inline void get_viewer_pose(pcl::visualization::PCLVisualizer::Ptr& viewe
   Eigen::Affine3f pose_af_inv = pose_af.inverse();
   for (int i = 0; i < 4; ++i)
   {
-    view_pose(i,0) = pose_af_inv(i,0);
-    view_pose(i,1) = pose_af_inv(i,1);
-    view_pose(i,2) = pose_af_inv(i,2);
-    view_pose(i,3) = pose_af_inv(i,3);
+	view_pose(i,0) = pose_af_inv(i,0);
+	view_pose(i,1) = pose_af_inv(i,1);
+	view_pose(i,2) = pose_af_inv(i,2);
+	view_pose(i,3) = pose_af_inv(i,3);
   }
   // // rotate along z-axis by 180
   Eigen::Matrix4f z_180_m = Eigen::Matrix4f::Identity();
@@ -102,28 +133,28 @@ static inline void get_viewer_pose(pcl::visualization::PCLVisualizer::Ptr& viewe
 
 void CallBackFunc(int event, int x, int y, int flags, void* userdata)
 {
-     if  ( event == cv::EVENT_LBUTTONDOWN )
-     {
-          // cout << "Left button of the mouse is clicked - position (" << x << ", " << y << ")" << endl;
-          cv::Point pt {x,y};
-          img_pts.push_back(pt);
-          proj_img_copy = proj_img.clone();
-          drawPoints(proj_img_copy, img_pts);
-     }
-     else if  ( event == cv::EVENT_RBUTTONDOWN )
-     {
-          cout << "Right button of the mouse is clicked - position (" << x << ", " << y << ")" << endl;
-     }
-     else if  ( event == cv::EVENT_MBUTTONDOWN )
-     {
-          cout << "Middle button of the mouse is clicked - position (" << x << ", " << y << ")" << endl;
-     }
-      else if  ( event == 'x' )
-     {
-          cout << "Middle button of the mouse is clicked - position (" << x << ", " << y << ")" << endl;
-     }
+	 if  ( event == cv::EVENT_LBUTTONDOWN )
+	 {
+		  // cout << "Left button of the mouse is clicked - position (" << x << ", " << y << ")" << endl;
+		  cv::Point pt {x,y};
+		  img_pts.push_back(pt);
+		  proj_img_copy = proj_img.clone();
+		  drawPoints(proj_img_copy, img_pts);
+	 }
+	 else if  ( event == cv::EVENT_RBUTTONDOWN )
+	 {
+		  cout << "Right button of the mouse is clicked - position (" << x << ", " << y << ")" << endl;
+	 }
+	 else if  ( event == cv::EVENT_MBUTTONDOWN )
+	 {
+		  cout << "Middle button of the mouse is clicked - position (" << x << ", " << y << ")" << endl;
+	 }
+	  else if  ( event == 'x' )
+	 {
+		  cout << "Middle button of the mouse is clicked - position (" << x << ", " << y << ")" << endl;
+	 }
 
-     cv::imshow("My Window", proj_img_copy);
+	 cv::imshow("My Window", proj_img_copy);
 }
 
 
@@ -142,39 +173,39 @@ void trigger_cv_window()
   //show the image
   while (1)
   {
-    // SEE https://progtpoint.blogspot.com/2017/06/key-board-ascii-key-code.html
+	// SEE https://progtpoint.blogspot.com/2017/06/key-board-ascii-key-code.html
 
-    cv::imshow("My Window", proj_img_copy);
+	cv::imshow("My Window", proj_img_copy);
 
-    char key = (char) cv::waitKey(10); 
-    
-    if (key == 'c') // backspace
-    {
-      cout << "C PRESSED\n";
-      img_pts.clear();
+	char key = (char) cv::waitKey(10); 
+	
+	if (key == 'c') // backspace
+	{
+	  cout << "C PRESSED\n";
+	  img_pts.clear();
 
-      proj_img_copy = proj_img.clone();
-      drawPoints(proj_img_copy, img_pts);
-    }
-    else if (key == 8) // backspace
-    {
-      cout << "BACKSPACE PRESSED\n";
-      if (img_pts.size() == 0)
-        continue;
-      img_pts.pop_back();
-      // show again
-      proj_img_copy = proj_img.clone();
-      drawPoints(proj_img_copy, img_pts);
-    }
-    else if (key == 13) // enter
-    {
-      cout << "ENTER PRESSED\n";
-      break;
-    }
-    else if (key == 'q')
-    {
-      break;
-    }
+	  proj_img_copy = proj_img.clone();
+	  drawPoints(proj_img_copy, img_pts);
+	}
+	else if (key == 8) // backspace
+	{
+	  cout << "BACKSPACE PRESSED\n";
+	  if (img_pts.size() == 0)
+		continue;
+	  img_pts.pop_back();
+	  // show again
+	  proj_img_copy = proj_img.clone();
+	  drawPoints(proj_img_copy, img_pts);
+	}
+	else if (key == 13) // enter
+	{
+	  cout << "ENTER PRESSED\n";
+	  break;
+	}
+	else if (key == 'q')
+	{
+	  break;
+	}
   }
 
   cv::destroyWindow("My Window");
@@ -183,11 +214,14 @@ void trigger_cv_window()
 cv::Mat get_mask()
 {
 	cv::Mat mask = cv::Mat::zeros(img_height, img_width, CV_8UC1);
-	const cv::Point* elementPoints[1] = { &img_pts[0] };
-	int num_pts = img_pts.size();
-	cv::fillPoly(mask, elementPoints, &num_pts, 1, 255);
-	// cv::imshow("mask", mask);
-	// cv::waitKey(0);
+	if (img_pts.size() > 0)
+	{
+		const cv::Point* elementPoints[1] = { &img_pts[0] };
+		int num_pts = img_pts.size();
+		cv::fillPoly(mask, elementPoints, &num_pts, 1, 255);
+		// cv::imshow("mask", mask);
+		// cv::waitKey(0);
+	}
 
 	return mask;
 }
@@ -213,13 +247,13 @@ void get_mask_3d_points(const cv::Mat& mask, std::vector<int>& cloud_indices)
 
 pcl::PointCloud<PointT>::Ptr extract_cloud(pcl::PointCloud<PointT>::Ptr cloud, pcl::PointIndices::Ptr indices, bool negative=false)
 {
-    pcl::PointCloud<PointT>::Ptr cloud_filtered(new pcl::PointCloud<PointT>);
-    pcl::ExtractIndices<PointT> extract;
-    extract.setInputCloud(cloud);
-    extract.setIndices(indices);
-    extract.setNegative(negative);
-    extract.filter(*cloud_filtered);
-    return cloud_filtered;
+	pcl::PointCloud<PointT>::Ptr cloud_filtered(new pcl::PointCloud<PointT>);
+	pcl::ExtractIndices<PointT> extract;
+	extract.setInputCloud(cloud);
+	extract.setIndices(indices);
+	extract.setNegative(negative);
+	extract.filter(*cloud_filtered);
+	return cloud_filtered;
 }
 
 pcl::PointCloud<PointT>::Ptr extract_cloud(pcl::PointCloud<PointT>::Ptr cloud, std::vector<int> indices, bool negative=false)
@@ -235,46 +269,92 @@ void keyboardEventOccurred(const pcl::visualization::KeyboardEvent &event, void*
   boost::shared_ptr<pcl::visualization::PCLVisualizer> viewer = *static_cast<boost::shared_ptr<pcl::visualization::PCLVisualizer> *>(viewer_void);
 
   std::string key = event.getKeySym();
+
+  bool refresh_cloud = false;
   if (event.keyDown())
   {
-  	if (key == "e" || key == "d")
-  	{
-	    get_viewer_pose(viewer, viewer_pose);
-	 	if (key == "e")
-	 	{
-	 		printf("Operation: EDIT\n");
-	 	} else 
-	 	{
-	 		printf("Operation: DELETE\n");
-	 	}
-	    trigger_cv_window();
-	    cv::Mat mask = get_mask();
-	    std::vector<int> cloud_indices;
-	    get_mask_3d_points(mask, cloud_indices);
-	    if (cloud_indices.size() == 0)
-	    {
-	    	return;
-	    }
-	 	if (key == "e")
-	 	{
-		    auto color = PclViewerUtils::get_random_color();
-		    PclViewerUtils::color_cloud_points(*cloud, cloud_indices, color);
-	 	} else {
-	 		auto& pts = cloud->points;
-	 		cloud = extract_cloud(cloud, cloud_indices, true);
-	 		// std::sort(cloud_indices.begin(), cloud_indices.end()); // sort so that erase can be ordered
-	 		// for (int i = 0; i < cloud_indices.size(); ++i)
-	 		// {
-	 		// 	int idx = cloud_indices[i] - i;
-	 		// 	pts.erase(pts.begin() + idx);
-	 		// }
-	 	}
-    	img_pts.clear();
+	bool is_ctrl = event.isCtrlPressed();
+	if (key == "a" || key == "d" || key == "x")
+	{
+		get_viewer_pose(viewer, viewer_pose);
+		if (key == "a")
+		{
+			printf("Operation: EDIT\n");
+		} else if (key == "d")
+		{
+			printf("Operation: DELETE\n");
+		} else 
+		{
+			printf("Operation: EXTRACT\n");
+		}
+		trigger_cv_window();
+		cv::Mat mask = get_mask();
+		std::vector<int> cloud_indices;
+		get_mask_3d_points(mask, cloud_indices);
+		if (cloud_indices.size() == 0)
+		{
+			return;
+		}
 
-	    viewer->removeAllPointClouds();
-	    viewer->addPointCloud(cloud);
-	    viewer->spin();
-  	}
+		// add current cloud to undo stack
+		pcl::PointCloud<PointT>::Ptr cur_cloud (new pcl::PointCloud<PointT>);
+		*cur_cloud += *cloud;
+		undo_clouds.push_back(cur_cloud);
+		redo_clouds.clear();
+
+		if (key == "a")
+		{
+			auto color = PclViewerUtils::get_random_color();
+			PclViewerUtils::color_cloud_points(*cloud, cloud_indices, color);
+		} else {
+			auto& pts = cloud->points;
+			if (key == "d")
+			{
+				cloud = extract_cloud(cloud, cloud_indices, true);
+			} else if (key == "x")
+			{
+				cloud = extract_cloud(cloud, cloud_indices, false);
+			}
+			// std::sort(cloud_indices.begin(), cloud_indices.end()); // sort so that erase can be ordered
+			// for (int i = 0; i < cloud_indices.size(); ++i)
+			// {
+			// 	int idx = cloud_indices[i] - i;
+			// 	pts.erase(pts.begin() + idx);
+			// }
+		}
+		img_pts.clear();
+
+		refresh_cloud = true;
+	}
+	else if (key == "s")
+	{
+		if (save_file.size() > 0)
+		{
+			pcl::io::savePCDFileBinary(save_file, *cloud);
+			printf("Saved to %s\n", save_file.c_str());
+		} else {
+			printf("Please pass a valid file to save_file [-s] argument\n");
+		}
+	}
+	else if (key == "z" && is_ctrl)
+	{
+		printf("Ctrl+z PRESSED\n");		
+		undo();
+		refresh_cloud = true;
+	}
+	else if (key == "y" && is_ctrl)
+	{
+		printf("Ctrl+y PRESSED\n");		
+		redo();
+		refresh_cloud = true;
+	}
+
+	if (refresh_cloud)
+	{
+		viewer->removeAllPointClouds();
+		viewer->addPointCloud(cloud);
+		viewer->spin();
+	}
   }
 }
 
@@ -283,10 +363,10 @@ void mouseEventOccurred(const pcl::visualization::MouseEvent &event, void* viewe
   boost::shared_ptr<pcl::visualization::PCLVisualizer> viewer = *static_cast<boost::shared_ptr<pcl::visualization::PCLVisualizer> *>(viewer_void);
 
   if (event.getButton() == pcl::visualization::MouseEvent::LeftButton &&
-      event.getType() == pcl::visualization::MouseEvent::MouseButtonRelease)
+	  event.getType() == pcl::visualization::MouseEvent::MouseButtonRelease)
   {
-    // pcl::visualization::Camera cam;
-    // viewer->getCameraParameters(cam);
+	// pcl::visualization::Camera cam;
+	// viewer->getCameraParameters(cam);
   }
 }
 
@@ -297,14 +377,20 @@ int main(int argc, char *argv[])
 
   if (argc > 1)
   {
-    pcd_file = argv[1];
+	pcd_file = argv[1];
   }
 
   if ( pcl::io::loadPCDFile <PointT> (pcd_file, *raw_cloud) == -1)
   {
-    std::cout << "Cloud reading failed." << std::endl;
-    return (-1);
+	std::cout << "Cloud reading failed." << std::endl;
+	return (-1);
   }
+
+  pcl::console::parse (argc, argv, "-ores", octree_resolution);
+  pcl::console::parse (argc, argv, "-width", img_width);
+  pcl::console::parse (argc, argv, "-height", img_height);
+  pcl::console::parse (argc, argv, "-focal", focal);
+  pcl::console::parse (argc, argv, "-s", save_file);
 
   cloud = raw_cloud;
 
